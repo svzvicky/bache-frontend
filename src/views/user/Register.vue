3ecc7f6dfbe30db69cecda67d89e41f045e1b485
Mithun M <mmithunsai@gmail.com>
2024-03-04 23:48:48 +0530

    validates input fields

--- src/views/user/Register.vue
+++ src/views/user/Register.vue
@@ -6,20 +6,28 @@
     <div class="flex flex-col mb-2">
       <label for="name" class="mb-2">Name</label>
       <input
-        type="name"
+        type="text"
         id="name"
         name="name"
+        v-model.trim="name"
         class="border-2 border-black-100 border-solid rounded"
       />
+      <span v-if="v$.name.$invalid" class="text-red-400"
+        >oops! invalid name</span
+      >
     </div>
     <div class="flex flex-col mb-2">
-      <label for="email" class="mb-2">Email</label>
+      <label for="mail" class="mb-2">Email</label>
       <input
-        type="email"
-        id="email"
-        name="email"
+        type="mail"
+        id="mail"
+        name="mail"
+        v-model.trim="mail"
         class="border-2 border-black-100 border-solid rounded"
       />
+      <span v-if="v$.mail.$invalid" class="text-red-400"
+        >oops! invalid mail</span
+      >
     </div>
     <div class="flex flex-col mb-10">
       <label for="password" class="mb-2">Password</label>
@@ -27,8 +35,12 @@
         type="password"
         id="password"
         name="password"
+        v-model.trim="password"
         class="border-2 border-black-100 border-solid rounded"
       />
+      <span v-if="v$.password.$invalid" class="text-red-400"
+        >oops! invalid format</span
+      >
     </div>
     <div class="flex justify-center">
       <button
@@ -40,6 +52,26 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import useVuelidate from "@vuelidate/core";
+import { ref, computed } from "vue";
+import { alpha, email, helpers } from "@vuelidate/validators";
 
+const name = ref("");
+const mail = ref("");
+const password = ref("");
+const passwordValidator= helpers.regex(/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/);
+const validations = computed(() => ({
+  name: {
+    alpha,
+  },
+  mail: {
+    email,
+  },
+  password: {
+    passwordValidator
+  },
+}));
+const v$ = useVuelidate(validations, { name, mail,password });
+</script>
 <style></style>
